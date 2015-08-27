4831b67c992762b2c97e8b8f71dc0c7620604877
ChipMorris10 <goldcomac@mac.com>
2015-08-27 14:45:42 -0600

    completed Exercise #3

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ button.addEventListener("mouseenter", function(event) {
 // Exercise 1a.
 // Add a mouseleave function
 button.addEventListener("mouseleave", function(event) {
-    event.target.style.color = "pink";
+    event.target.style.color = "red";
     });
 });
 
@@ -21,42 +21,70 @@ myDiv.onmouseenter = function() {
 };
 
 myDiv.onmouseleave = function() {
-    event.target.style.color = "black";
+    event.target.style.color = "blue";
 };
 
 // ** How do I isolate the button by itself? **//
-// ** Error on line 5 in the console **//
+// ** console shows an error on line 5, but I don't know why **//
 
 // Exercise 2
-var x = document.getElementById("btn-bg");
-console.log(x);
+var changeColor = document.getElementById("btn-bg");
 
-x.addEventListener("click", function(event) {
+changeColor.addEventListener("click", function(event) {
     event.target.style.backgroundColor = "yellow";
 });
 
 // ** I'm supposed to change the background of the page. I tired **//
-// var x = document.getElementById("body");
-// console.log(x);
+// var changeColor = document.getElementById("body");
+// console.log(changeColor);
 
-// x.addEventListener("click", function(event) {
+// changeColor.addEventListener("click", function(event) {
 //     event.body.style.backgroundColor = "yellow";
 // });
 
 // Exercise 3
-var y = document.getElementById("container");
+var twoClicks = document.getElementById("container");
 
-y.addEventListener("dblclick", function(event) {
+twoClicks.addEventListener("dblclick", function(event) {
     alert("Check it out.\nTwo lines.");
 });
 
 // Exercise 4
-var z = document.getElementById("tofu");
+// var foodPicture = document.getElementById("tofu");
 
-z.addEventListener("click", function(event) {
+// foodPicture.addEventListener("click", function(event) {
+//     console.log(foodPicture);
 
-    for(var i = 0; i < 1000; i++) {
-        clone();
-    }
+//     for(var i = 0; i < 1000; i++) {
+//         clone();
+//     }
+// });
+
+// Exercise 5
+// Skip. Too much css according to Zach
+
+
+// Exercise 6
+// var changeLines = document.getElementById("list").children;
+
+// changeLineColor.addEventListener("click", function() {
+//     event.target.children.backgroundColor = (changeLineColor)[0],"red";
+// });
+
+
+    // var changeLines = document.getElemenById("list").children[0];
+
+    // changeLines.addEventListener("click", function(event) {
+    //     event.body.style.backgroundColor = "red";
+    // });
+
+
+// Exercise 7
+var newLineColor = document.getElementById("random");
+changeColor.addEventListener("click", function() {
+    event.target.style.color = "yellow";
 });
+
+
+
 
